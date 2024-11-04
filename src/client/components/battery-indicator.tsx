7527fdcbab628777348d7dd2114b2898cb621e3b
Javier Lurgo <jlurgo@gmail.com>
2024-11-04 17:22:15 -0300

    - refactored ROS subscriptions in a context manager - battery indicator subscribes using fleet context

--- src/client/components/battery-indicator.tsx
+++ src/client/components/battery-indicator.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import { CapabilityContext, CapabilityContextProvider } from '@transitive-sdk/utils-web'
+import React from 'react';
 import { useContext } from 'react';
-import { JWTContext, JWTContextProvider } from '@components/jwt-context';
 import { getLogger} from '@transitive-sdk/utils-web';
 import { BatteryWarning, BatteryCharging, BatteryFull, BatteryMedium, BatteryLow, Battery } from 'lucide-react';
+import { ROSContextInFleet, ROSContextProviderInFleet } from '@components/ros-provider';
 
 const log = getLogger('BatteryIndicator');
 log.setLevel('debug');
 
-
-const SimpleBatteryIndicator = ({deviceData}) => {
+//v4
+export const BatteryIndicatorWithContext = () => {
   // get the battery status from the reactively updating device data
+  const { deviceData } = useContext(ROSContextInFleet);
   const batteryStatus = deviceData?.ros?.[1].messages?.status?.battery;
 
   return <div>
@@ -24,38 +24,141 @@ const SimpleBatteryIndicator = ({deviceData}) => {
   </div>
 }
 
-const BatteryIndicatorProvidedWithCapability = () => {
-  // import the API exposed by the ros-tool capability
-  const { isReady, subscribe, unsubscribe, deviceData } = useContext(CapabilityContext)
-
-  useEffect(() => {
-    if (isReady?.()) {
-      // subscribe to a ROS 1 topic on the robot
-      subscribe(1, '/status/battery');
-    }
-
-    // unsubscribe when React component unmounts
-    return () => {
-      unsubscribe?.(1, '/status/battery');
-    }
-  }, [isReady, subscribe])
-  
-  return <SimpleBatteryIndicator deviceData={deviceData} />
-}
-
-const BatteryIndicatorProvidedWithJwt = () => {
-  const jwt = useContext(JWTContext);
-  return(
-    <CapabilityContextProvider jwt={jwt}>
-      <BatteryIndicatorProvidedWithCapability />
-    </CapabilityContextProvider>
-  );
-}
-
 export const BatteryIndicator = ({device}) => {
   return (
-    <JWTContextProvider device={device} capability={'@transitive-robotics/ros-tool'}>
-      <BatteryIndicatorProvidedWithJwt />
-    </JWTContextProvider>
-  );
-}
+    <ROSContextProviderInFleet device={device} ros1Topics={['/status/battery']}>
+      <BatteryIndicatorWithContext />
+    </ROSContextProviderInFleet>
+  )
+};
+
+
+// v3 - takes data from fleet context
+// export const BatteryIndicator = ({device}) => {
+//   // get the battery status from the reactively updating device data
+//   const context = useContext(CapabilityContext);
+//   const [deviceData, setDeviceData] = useState();
+
+//   useEffect(() => {
+//     log.debug('Context', context);
+//     const api = context.getAPI(device);
+//     log.debug('API', api);
+//     api?.subscribe(1, '/status/battery');
+//     setDeviceData(api?.deviceData);
+//     // unsubscribe when React component unmounts
+//     return () => {
+//       api?.unsubscribe?.(1, '/status/battery');
+//     }
+//   }, [context?.ready]);
+
+//   const batteryStatus = deviceData?.ros?.[1].messages?.status?.battery;
+//   if (batteryStatus) {
+//     log.debug('Battery status', batteryStatus.charge);
+//   }
+
+//   return <div>
+//     {!batteryStatus ? <BatteryWarning /> :
+//       batteryStatus.power_supply_status === 1 ? <BatteryCharging /> :
+//       batteryStatus.charge > 90 ? <BatteryFull /> :
+//       batteryStatus.charge <= 90 && batteryStatus.charge > 50 ? <BatteryMedium /> :
+//       batteryStatus.charge <= 50 && batteryStatus.charge > 20 ? <BatteryLow /> :
+//       <Battery />
+//     }
+//   </div>
+// }
+
+
+// v2
+// export const BatteryIndicatorWithContext = () => {
+//   // get the battery status from the reactively updating device data
+//   const {deviceData} = useContext(ROSContext);
+//   const batteryStatus = deviceData?.ros?.[1].messages?.status?.battery;
+//   if (batteryStatus) {
+//     log.debug('Battery status', batteryStatus.charge);
+//   }
+//   return <div>
+//     {!batteryStatus ? <BatteryWarning /> :
+//       batteryStatus.power_supply_status === 1 ? <BatteryCharging /> :
+//       batteryStatus.charge > 90 ? <BatteryFull /> :
+//       batteryStatus.charge <= 90 && batteryStatus.charge > 50 ? <BatteryMedium /> :
+//       batteryStatus.charge <= 50 && batteryStatus.charge > 20 ? <BatteryLow /> :
+//       <Battery />
+//     }
+//   </div>
+// }
+
+// export const BatteryIndicator = ({device}) => {
+//   return (
+//     <ROSContextProvider device={device} ros1Topics={['/status/battery']}>
+//       <BatteryIndicatorWithContext />
+//     </ROSContextProvider>
+//   )
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//v1
+// const SimpleBatteryIndicator = ({deviceData}) => {
+//   // get the battery status from the reactively updating device data
+//   const batteryStatus = deviceData?.ros?.[1].messages?.status?.battery;
+
+//   if (batteryStatus) {
+//     log.debug('Battery status', batteryStatus.charge);
+//   }
+//   return <div>
+//     {!batteryStatus ? <BatteryWarning /> :
+//       batteryStatus.power_supply_status === 1 ? <BatteryCharging /> :
+//       batteryStatus.charge > 90 ? <BatteryFull /> :
+//       batteryStatus.charge <= 90 && batteryStatus.charge > 50 ? <BatteryMedium /> :
+//       batteryStatus.charge <= 50 && batteryStatus.charge > 20 ? <BatteryLow /> :
+//       <Battery />
+//     }
+//   </div>
+// }
+
+// const BatteryIndicatorProvidedWithCapability = () => {
+//   // import the API exposed by the ros-tool capability
+//   const { isReady, subscribe, unsubscribe, deviceData } = useContext(CapabilityContext)
+
+//   useEffect(() => {
+//     if (isReady?.()) {
+//       // subscribe to a ROS 1 topic on the robot
+//       subscribe(1, '/status/battery');
+//     }
+
+//     // unsubscribe when React component unmounts
+//     return () => {
+//       unsubscribe?.(1, '/status/battery');
+//     }
+//   }, [isReady, subscribe])
+  
+//   return <SimpleBatteryIndicator deviceData={deviceData} />
+// }
+
+// const BatteryIndicatorProvidedWithJwt = () => {
+//   const jwt = useContext(JWTContext);
+//   return(
+//     <CapabilityContextProvider jwt={jwt}>
+//       <BatteryIndicatorProvidedWithCapability />
+//     </CapabilityContextProvider>
+//   );
+// }
+
+// export const BatteryIndicator = ({device}) => {
+//   return (
+//     <JWTContextProvider device={device} capability={'@transitive-robotics/ros-tool'}>
+//       <BatteryIndicatorProvidedWithJwt />
+//     </JWTContextProvider>
+//   );
+// }
---
+++ src/client/components/ros-provider.tsx
@@ -0,0 +1,84 @@
+import React, {  createContext, useEffect, useContext, useState } from 'react';
+import { CapabilityContext, CapabilityContextProvider } from '@transitive-sdk/utils-web'
+import { _ } from 'lodash';
+import { JWTContext, JWTContextProvider } from '@components/jwt-context';
+import { getLogger} from '@transitive-sdk/utils-web';
+
+const log = getLogger('ROSProvider');
+log.setLevel('debug');
+
+
+export const ROSContext = createContext({});
+
+const ProviderWithROS = ({ children, ros1Topics }) => {
+  // import the API exposed by the ros-tool capability
+  const { isReady, subscribe, unsubscribe, deviceData, getApi} = useContext(CapabilityContext)
+
+  // log.debug('ProviderWithROS', isReady, subscribe, unsubscribe, deviceData, getApi);
+  
+  useEffect(() => {
+    if (isReady?.()) {
+      // subscribe to a ROS 1 topic on the robot
+      _.forEach(ros1Topics, (topic, i) => {
+        log.debug('Subscribing to topic', topic);
+        subscribe?.(1, topic);
+      });
+    }
+
+    // unsubscribe when React component unmounts
+    return () => {
+      _.forEach(ros1Topics, (topic, i) => {
+        log.debug('Unsubscribing from topic', topic);
+        unsubscribe?.(1, topic);
+      });
+    }
+  }, [isReady, subscribe]);
+
+  return <ROSContext.Provider value={{deviceData}}>{children}</ROSContext.Provider>
+}
+
+const ProviderWithJwt = ({children}) => {
+  const jwt = useContext(JWTContext);
+  return(
+    <CapabilityContextProvider jwt={jwt}>
+      {children}
+    </CapabilityContextProvider>
+  );
+}
+
+export const ROSContextProvider = ({ children, device, ros1Topics }) => {
+  log.debug('ROSContextProvider', device, ros1Topics);
+  return <JWTContextProvider device={device} capability={'@transitive-robotics/ros-tool'}>
+    <ProviderWithJwt>
+      <ProviderWithROS ros1Topics={ros1Topics}>
+        {children}
+      </ProviderWithROS>
+    </ProviderWithJwt>
+  </JWTContextProvider>
+};
+
+
+export const ROSContextInFleet = createContext({});
+export const ROSContextProviderInFleet = ({ children, device, ros1Topics }) => {
+  // get the battery status from the reactively updating device data
+  const capabilityContext = useContext(CapabilityContext);
+  const [deviceData, setDeviceData] = useState();
+
+  useEffect(() => {
+    const api = capabilityContext.getAPI(device);
+    api?.subscribe && _.forEach(ros1Topics, (topic, i) => {
+      log.debug('Subscribing to topic', topic);
+      api.subscribe(1, topic);
+    });
+    setDeviceData(api?.deviceData);
+    // unsubscribe when React component unmounts
+    return () => {
+      api?.unsubscribe && _.forEach(ros1Topics, (topic, i) => {
+        log.debug('Unsubscribing from topic', topic);
+        api.unsubscribe(1, topic);
+      });
+    }
+  }, [capabilityContext?.ready]);
+
+  return <ROSContextInFleet.Provider value={{deviceData}}>{children}</ROSContextInFleet.Provider>
+}
